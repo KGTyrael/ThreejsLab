23c054dabb2e244f9f48f85e05c46c6891a99860
KGTyrael <43983032+KGTyrael@users.noreply.github.com>
2020-11-12 02:10:42 +0800

    Merge branch 'main' of https://gitee.com/KGTyrael/threejsLab into main
    # Conflicts:
    #	dist/client/client.js

--- dist/client/client.js
+++ dist/client/client.js
@@ -10,8 +10,8 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-camera.position.z = 3;
-var animate = function() {
+camera.position.z = 2;
+var animate = function () {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
