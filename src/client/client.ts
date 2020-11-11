dac6c38d1ee87b1499ca32f33bbaf72a0729d72f
KGTyrael <43983032+KGTyrael@users.noreply.github.com>
2020-11-12 02:39:06 +0800

    Add Stats Panel

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
+import Stats from '/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -10,6 +11,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change', render) 
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -19,15 +21,32 @@ scene.add(cube)
 
 camera.position.z = 2
 
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 var animate = function () {
     requestAnimationFrame(animate)
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    controls.update()
+    render()
 
-    renderer.render(scene, camera)
+    stats.update()
 };
 
+function render() {
+    stats.begin()
+    renderer.render(scene, camera)
+    stats.end()
+}
+//render()
 animate();
